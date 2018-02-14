e0bfb98ed2f4d9062d0dc1ab7abf2c3bc332f5ed
samory harris <samory070@gmail.com>
2018-02-14 18:26:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -27,6 +27,8 @@ var mixedCase = 'Hi there!'
 let sayHiToGrandma = function(string) {
   if(lowercase.toLowerCase() === lowercase) {
     return "I can\'t hear you!"
+  } else if (true) {
+
   }
 }
 sayHiToGrandma('hello')
