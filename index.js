99ed4cb69c00836230f63f327da7a8202aea42ae
samory harris <samory070@gmail.com>
2018-02-14 18:16:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,3 +12,8 @@ let logShout = function(string) {
   console.log(string.toUpperCase())
 }
 logShout('HELLO')
+
+logWhisper = function(string) {
+  console.log(string.toLowerCase())
+}
+logWhisper('HELLO')
